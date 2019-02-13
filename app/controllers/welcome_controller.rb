da05a63eaec9069d2a76861dfa61171e31e006b4
nevermore2014 <zhou.hongyu8976@gmail.com>
2019-02-13 14:51:50 -0500

    Setting up 1 st level redirection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def case
+  end
+
+  def company
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/company'
+  get 'welcome/products'
+  get 'welcome/news'
+  get 'welcome/factory'
+  get 'welcome/case'
+  get 'welcome/contact_us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
