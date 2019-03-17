377608aae97c44508f500ee291679e008f5ac662
nevermore2014 <zhou.hongyu8976@gmail.com>
2019-03-17 14:40:42 -0400

    shipin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -30,7 +30,7 @@ class WelcomeController < ApplicationController
 
   def robots
     robots = File.read(Rails.root + "config/robots.#{Rails.env}.txt")
-    render :text => robots, :layout => false, :content_type => "text/plain"
+    render plain: robots
   end
 
 end
